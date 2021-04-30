3dc15cc79547923884859ad6a724a1c5b62f0c4f
Vheos <Vheos777@gmail.com>
2021-04-30 19:35:20 +0200

    bugfix: extra gamepad quickslots had IDelayedInit

--- Mods/Gamepad.cs
+++ Mods/Gamepad.cs
@@ -11,7 +11,7 @@ using UnityEngine.EventSystems;
 
 namespace ModPack
 {
-    public class Gamepad : AMod, IDelayedInit, IUpdatable
+    public class Gamepad : AMod, IUpdatable
     {
         // Setting
         static private ModSetting<bool> _extraGamepadQuickslots;
