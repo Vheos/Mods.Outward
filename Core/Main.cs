f91723f7848527a3d81b2c79e5b2695c93f7a99a
Vheos <Vheos777@gmail.com>
2021-05-01 17:34:33 +0200

    1.7.0 RC

--- Core/Main.cs
+++ Core/Main.cs
@@ -15,7 +15,7 @@ namespace ModPack
         // Settings
         public const string GUID = "com.Vheos.ModPack";
         public const string NAME = "Vheos Mod Pack";
-        public const string VERSION = "1.6.1";
+        public const string VERSION = "1.7.0";
 
         // Utility
         private List<Type> _awakeModTypes;
