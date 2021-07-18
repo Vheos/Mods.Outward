8194b89799379364ea9c6af0fd2b217f2aed7919
Vheos <Vheos777@gmail.com>
2021-07-18 03:46:51 +0200

    added BepInExEntryPoint inheritance

--- Core/Main.cs
+++ Core/Main.cs
@@ -8,6 +8,7 @@ using Vheos.Tools.ModdingCore;
 using BepInEx;
 using Vheos.Tools.Extensions.General;
 using Vheos.Tools.Extensions.Collections;
+using System.Reflection;
 
 
 
@@ -16,14 +17,13 @@ namespace ModPack
     [BepInDependency("com.bepis.bepinex.configurationmanager", BepInDependency.DependencyFlags.SoftDependency)]
     [BepInDependency("io.mefino.configurationmanager", BepInDependency.DependencyFlags.SoftDependency)]
     [BepInPlugin(GUID, NAME, VERSION)]
-    public class Main : BaseUnityPlugin
+    public class Main : BepInExEntryPoint
     {
         #region SETTINGS
         public const bool IS_DEVELOPMENT_VERSION = false;
         public const string GUID = "Vheos.Mods.Outward";
         public const string NAME = "Vheos Mod Pack" + (IS_DEVELOPMENT_VERSION ? " [DEVELOPMENT]" : "");
         public const string VERSION = "1.13.0";
-        static public Type[] BLACKLIST = { typeof(Debug), typeof(WIP), typeof(PistolTweaks) };
         static private readonly Type[] MODS_ORDERING_LIST = new[]
         {
             // Survival & Immersion
@@ -69,97 +69,27 @@ namespace ModPack
         };
         #endregion
 
-        // Utility
-        private List<Type> _awakeModTypes;
-        private List<Type> _delayedModTypes;
-        private List<IUpdatable> _updatableMods;
-        private List<AMod> _mods;
-        private void CategorizeModsByInstantiationTime(Type[] whitelist = null, Type[] blacklist = null)
+        // User logic
+        override public Assembly CurrentAssembly
+        => Assembly.GetExecutingAssembly();       
+        override public void Initialize()
         {
-            foreach (var modType in Utility.GetDerivedTypes<AMod>())
-                if ((blacklist.IsNullOrEmpty() || modType.IsNotContainedIn(blacklist))
-                && (whitelist.IsNullOrEmpty() || modType.IsContainedIn(whitelist)))
-                    if (modType.IsAssignableTo<IDelayedInit>())
-                        _delayedModTypes.Add(modType);
-                    else
-                        _awakeModTypes.Add(modType);
-        }
-        private void TryDelayedInitialize()
-        {
-            if (Prefabs.IsInitialized || !IsGameInitialized)
-                return;
-
-            Tools.Log($"Finished waiting");
-            Tools.Log("");
-
-            Tools.Log("Initializing prefabs...");
-            Prefabs.Initialize();
-            Tools.Log("Instantiating delayed mods...");
-            InstantiateMods(_delayedModTypes);
-
-            Tools.Log("Initializing Presets...");
-            Presets.Initialize(_mods);
-
-            Tools.Log($"Finished DelayedInit");
-        }
-        private void InstantiateMods(ICollection<Type> modTypes)
-        {
-            foreach (var modType in modTypes)
-            {
-                AMod newMod = (AMod)Activator.CreateInstance(modType);
-                _mods.Add(newMod);
-                if (modType.IsAssignableTo<IUpdatable>())
-                    _updatableMods.Add(newMod as IUpdatable);
-            }
-        }
-        private void UpdateMods(ICollection<IUpdatable> updatableMods)
-        {
-            foreach (var updatableMod in updatableMods)
-                if (updatableMod.IsEnabled)
-                    updatableMod.OnUpdate();
-        }
-        private bool IsGameInitialized
-        => ResourcesPrefabManager.Instance.Loaded && UIUtilities.m_instance != null;
-
-        // Mono
-#pragma warning disable IDE0051 // Remove unused private members
-        private void Awake()
-        {
-            _awakeModTypes = new List<Type>();
-            _delayedModTypes = new List<Type>();
-            _updatableMods = new List<IUpdatable>();
-            _mods = new List<AMod>();
-
             AMod.SetOrderingList(MODS_ORDERING_LIST);
-            Tools.Initialize(this, Logger);
-
             Tools.Log("Initializing GameInput...");
             GameInput.Initialize();
             Tools.Log("Initializing Players...");
             Players.Initialize();
-
-            Tools.Log("Categorizing mods by instantiation time...");
-            CategorizeModsByInstantiationTime(null, BLACKLIST);
-            Tools.Log("Awake:");
-            foreach (var modType in _awakeModTypes)
-                Tools.Log($"\t{modType.Name}");
-            Tools.Log("Delayed:");
-            foreach (var modType in _delayedModTypes)
-                Tools.Log($"\t{modType.Name}");
-
-            Tools.Log("Instantiating awake mods...");
-            InstantiateMods(_awakeModTypes);
-
-            Tools.Log($"Finished AwakeInit");
-            Tools.Log("");
-
-            Tools.Log($"Waiting for game initialization...");
         }
-        private void Update()
+        override public void DelayedInitialize()
         {
-            TryDelayedInitialize();
-            UpdateMods(_updatableMods);
-            Tools.TryRedrawConfigWindow();
+            Tools.Log("Initializing Prefabs...");
+            Prefabs.Initialize();
+            Tools.Log("Initializing Presets...");
+            Presets.Initialize(_mods);
         }
+        override public bool DelayedInitializeCondition
+        => ResourcesPrefabManager.Instance.Loaded && UIUtilities.m_instance != null;
+        override public Type[] Blacklist
+        => new[] { typeof(Debug), typeof(WIP), typeof(PistolTweaks) };
     }
 }
