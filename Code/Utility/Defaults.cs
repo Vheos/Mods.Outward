5549168eb87fca4fdcf5e77ce5ec01fc9723b8d4
Vheos <Vheos777@gmail.com>
2022-06-22 01:16:05 +0200

    Imported fields from `Lists` and `Various`

--- Code/Utility/Defaults.cs
+++ Code/Utility/Defaults.cs
@@ -6,6 +6,26 @@ public static class Defaults
     public const int ArmorTrainingPenaltyReduction = 50;
     public const float BaseStaminaRegen = 2.4f;
     public const int InnRentTime = 12;
+    public const string InnQuestsFamilyName = "Inns";
+
+    public static readonly AreaManager.AreaEnum[] Regions = new[]
+{
+        AreaManager.AreaEnum.CierzoOutside,
+        AreaManager.AreaEnum.Emercar,
+        AreaManager.AreaEnum.HallowedMarsh,
+        AreaManager.AreaEnum.Abrassar,
+        AreaManager.AreaEnum.AntiqueField,
+        AreaManager.AreaEnum.Caldera,
+    };
+    public static readonly AreaManager.AreaEnum[] Cities = new[]
+    {
+        AreaManager.AreaEnum.CierzoVillage,
+        AreaManager.AreaEnum.Berg,
+        AreaManager.AreaEnum.Monsoon,
+        AreaManager.AreaEnum.Levant,
+        AreaManager.AreaEnum.Harmattan,
+        AreaManager.AreaEnum.NewSirocco,
+    };
     public static readonly Dictionary<TemperatureSteps, Vector2> TemperateDataByStep = new()
     {
         [TemperatureSteps.Hottest] = new Vector2(40, 101),
