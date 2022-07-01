6f58e66c2dccfd6b026c61a4aecdfb91385b36ae
Vheos <Vheos777@gmail.com>
2022-07-01 02:02:14 +0200

    Refreshed mod `Dodge`

--- Code/Mods/Combat/Dodge.cs
+++ Code/Mods/Combat/Dodge.cs
@@ -2,45 +2,31 @@
 
 public class Dodge : AMod
 {
-
-    // Settings
+    #region Settings
     private static ModSetting<int> _staminaCost;
-    private static ModSetting<int> _staminaCostAcrobatics;
+    private static ModSetting<int> _staminaCostWithAcrobatics;
     private static ModSetting<bool> _allowMidAttack;
     private static ModSetting<bool> _allowMidAttackUntilDamageDealt;
-    private static ModSetting<bool> _allowMidAttackeUntilDamageTaken;
-    private static ModSetting<bool> _removeInvulnerability;
+    private static ModSetting<bool> _allowMidAttackUntilDamageTaken;
+    private static ModSetting<bool> _invincibility;
     protected override void Initialize()
     {
         _staminaCost = CreateSetting(nameof(_staminaCost), 6, IntRange(0, 50));
-        _staminaCostAcrobatics = CreateSetting(nameof(_staminaCostAcrobatics), 9, IntRange(0, 50));
+        _staminaCostWithAcrobatics = CreateSetting(nameof(_staminaCostWithAcrobatics), 9, IntRange(0, 50));
         _allowMidAttack = CreateSetting(nameof(_allowMidAttack), false);
-        _allowMidAttackUntilDamageDealt = CreateSetting(nameof(_allowMidAttackeUntilDamageTaken), false);
-        _allowMidAttackeUntilDamageTaken = CreateSetting(nameof(_allowMidAttackUntilDamageDealt), false);
-        _removeInvulnerability = CreateSetting(nameof(_removeInvulnerability), false);
+        _allowMidAttackUntilDamageDealt = CreateSetting(nameof(_allowMidAttackUntilDamageTaken), false);
+        _allowMidAttackUntilDamageTaken = CreateSetting(nameof(_allowMidAttackUntilDamageDealt), false);
+        _invincibility = CreateSetting(nameof(_invincibility), true);
     }
-    protected override void SetFormatting()
-    {
-        _staminaCost.Format("Stamina cost");
-        using (Indent)
-        {
-            _staminaCostAcrobatics.Format("with acrobatics");
-        }
-        _allowMidAttack.Format("Allow mid-attack dodge");
-        using (Indent)
-        {
-            _allowMidAttackUntilDamageDealt.Format("until you deal damage", _allowMidAttack);
-            _allowMidAttackeUntilDamageTaken.Format("until you take damage", _allowMidAttack);
-        }
-        _removeInvulnerability.Format("Remove dodge invulnerability");
-        _removeInvulnerability.Description =
-            "You can get hit during the dodge animation\n" +
-            "(even without a backpack)";
-    }
-    protected override string Description
-    => "";
+    #endregion
+
+    #region Formatting
     protected override string SectionOverride
-    => ModSections.Combat;
+=> ModSections.Combat;
+    protected override string Description
+        => "• Adjust stamina cost" +
+        "\n• Allow dodging during attack animation" +
+        "\n• Remove dodge invincibility";
     protected override void LoadPreset(string presetName)
     {
         switch (presetName)
@@ -48,17 +34,48 @@ public class Dodge : AMod
             case nameof(Preset.Vheos_CoopSurvival):
                 ForceApply();
                 _staminaCost.Value = 6;
-                _staminaCostAcrobatics.Value = 9;
+                _staminaCostWithAcrobatics.Value = 9;
                 _allowMidAttack.Value = true;
                 _allowMidAttackUntilDamageDealt.Value = true;
-                _allowMidAttackeUntilDamageTaken.Value = true;
-                _removeInvulnerability.Value = true;
+                _allowMidAttackUntilDamageTaken.Value = true;
+                _invincibility.Value = false;
                 break;
         }
     }
+    protected override void SetFormatting()
+    {
+        _staminaCost.Format("Stamina cost");
+        _staminaCost.Description =
+            "How much stamina dodging costs" +
+            "\n\nUnit: stamina points";
+        using (Indent)
+        {
+            _staminaCostWithAcrobatics.Format("with acrobatics");
+            _staminaCostWithAcrobatics.Description =
+                "How much stamina dodging costs when you have the Acrobatics passive skill" +
+                "\n\nUnit: stamina points";
+        }
+        _allowMidAttack.Format("Allow mid-attack");
+        _allowMidAttack.Description =
+            "Allows you to dodge even if you're in the middle of an attack animation";
+        using (Indent)
+        {
+            _allowMidAttackUntilDamageDealt.Format("until you deal damage", _allowMidAttack);
+            _allowMidAttackUntilDamageDealt.Description =
+                "Prevents using mid-attack dodge after you deal damage" +
+                "\nLasts only until you start a new attack";
+            _allowMidAttackUntilDamageTaken.Format("until you take damage", _allowMidAttack);
+            _allowMidAttackUntilDamageTaken.Description =
+                "Prevents using mid-attack dodge after you take damage" +
+                "\nLasts only until you start a new attack";
+        }
+        _invincibility.Format("Invincibility");
+        _invincibility.Description =
+            "Makes you invincible for roughly 500ms while performing a normal dodge (not slowed down by backpack)";
+    }
+    #endregion
 
-
-    // Hooks
+    #region Hooks
     // Mid-attack dodge
     [HarmonyPostfix, HarmonyPatch(typeof(Character), nameof(Character.StartAttack))]
     private static void Character_StartAttack_Post(Character __instance, int _type, int _id)
@@ -76,7 +93,7 @@ public class Dodge : AMod
         if (!_allowMidAttack)
             return;
 
-        if (_allowMidAttackeUntilDamageTaken
+        if (_allowMidAttackUntilDamageTaken
         && __instance != null
         && __instance.IsPlayer())
             __instance.m_dodgeAllowedInAction = 0;
@@ -96,10 +113,8 @@ public class Dodge : AMod
     [HarmonyPostfix, HarmonyPatch(typeof(Character), nameof(Character.DodgeStep))]
     private static void Character_DodgeStep_Post(ref Hitbox[] ___m_hitboxes, ref int _step)
     {
-        #region quit
-        if (!_removeInvulnerability)
+        if (_invincibility)
             return;
-        #endregion
 
         if (_step > 0 && ___m_hitboxes != null)
             foreach (var hitbox in ___m_hitboxes)
@@ -108,8 +123,8 @@ public class Dodge : AMod
 
     [HarmonyPostfix, HarmonyPatch(typeof(Character), nameof(Character.DodgeStamCost), MethodType.Getter)]
     private static void Character_DodgeStamCost_Getter_Post(Character __instance, ref int __result)
-    => __result = __instance.Inventory.SkillKnowledge.IsItemLearned(ACROBATICS_SKILLID)
-        ? _staminaCostAcrobatics
+        => __result = __instance.Inventory.SkillKnowledge.IsItemLearned("Acrobatics".ToSkillID())
+        ? _staminaCostWithAcrobatics
         : _staminaCost;
-
+    #endregion
 }
