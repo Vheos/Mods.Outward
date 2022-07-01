60b72c0a201db3b9afa62c1dd07727bd55295d2f
Vheos <Vheos777@gmail.com>
2022-07-01 03:13:40 +0200

    Refreshed mod `Traps`

--- Code/Mods/Combat/Traps.cs
+++ Code/Mods/Combat/Traps.cs
@@ -2,69 +2,78 @@
 
 public class Traps : AMod
 {
-    #region Constants
-    private static Color TRAP_START_COLOR = Color.white;
-    private static Color TRAP_TRANSITION_COLOR = Color.yellow;
-    private static Color TRAP_ARMED_COLOR = Color.red;
-    private static Color RUNIC_TRAP_START_COLOR = new(1f, 1f, 1f, 0f);
-    private static Color RUNIC_TRAP_TRANSITION_COLOR = new(1f, 1f, 0.05f, 0.05f);
-    private static Color RUNIC_TRAP_ARMED_COLOR = new(1f, 0.05f, 0f, 1f);
-    #endregion
-
-    // Settings
-    private static ModSetting<float> _trapsArmDelay;
-    private static ModSetting<bool> _trapsFriendlyFire;
+    #region Settings
+    private static ModSetting<float> _armingDuration;
+    private static ModSetting<bool> _friendlyFire;
     private static ModSetting<float> _pressureTrapRadius, _wireTrapDepth, _runicTrapRadius;
     protected override void Initialize()
     {
-
-        _trapsArmDelay = CreateSetting(nameof(_trapsArmDelay), 0f, FloatRange(0f, 5f));
-        _trapsFriendlyFire = CreateSetting(nameof(_trapsFriendlyFire), false);
+        _armingDuration = CreateSetting(nameof(_armingDuration), 0f, FloatRange(0f, 10f));
+        _friendlyFire = CreateSetting(nameof(_friendlyFire), false);
         _wireTrapDepth = CreateSetting(nameof(_wireTrapDepth), 0.703f, FloatRange(0f, 5f));
         _pressureTrapRadius = CreateSetting(nameof(_pressureTrapRadius), 1.1f, FloatRange(0f, 5f));
         _runicTrapRadius = CreateSetting(nameof(_runicTrapRadius), 2.5f, FloatRange(0f, 5f));
 
-        _trapsArmDelay.AddEvent(() =>
+        _armingDuration.AddEvent(() =>
         {
-            if (_trapsArmDelay == 0)
-                _trapsFriendlyFire.Value = false;
+            if (_armingDuration == 0)
+                _friendlyFire.Value = false;
         });
     }
-    protected override void SetFormatting()
-    {
-        _trapsArmDelay.Format("Traps arming delay");
-        _trapsArmDelay.Description = "How long the trap has to stay on ground before it can explode (in seconds)";
-        using (Indent)
-        {
-            _trapsFriendlyFire.Format("Friendly fire", _trapsArmDelay, t => t > 0);
-            _trapsFriendlyFire.Description = "The trap will also explode in contact with you and other players";
-        }
-        _wireTrapDepth.Format("Tripwire trap trigger depth");
-        _pressureTrapRadius.Format("Presure plate trigger radius");
-        _runicTrapRadius.Format("Runic trap trigger radius");
-    }
-    protected override string Description
-    => "• Set a delay before a trap can explode\n" +
-       "• Make traps explode in contact with players\n" +
-       "• Change trigger size for each trap";
-    protected override string SectionOverride
-    => ModSections.Combat;
     protected override void LoadPreset(string presetName)
     {
         switch (presetName)
         {
             case nameof(Preset.Vheos_CoopSurvival):
                 ForceApply();
-                _trapsArmDelay.Value = 5;
-                _trapsFriendlyFire.Value = false;
+                _armingDuration.Value = 5;
+                _friendlyFire.Value = false;
                 _wireTrapDepth.Value = 0.2f;
                 _pressureTrapRadius.Value = 0.6f;
                 _runicTrapRadius.Value = 0.8f;
                 break;
         }
     }
+    #endregion
 
-    // Utility
+    #region Formatting
+    protected override string SectionOverride
+        => ModSections.Combat;
+    protected override string Description
+        => "• Set a delay before a trap can get triggered" +
+        "\n• Allow traps to damage players" +
+        "\n• Adjust trigger size for each trap";
+    protected override void SetFormatting()
+    {
+        _armingDuration.Format("Arming duration");
+        _armingDuration.Description =
+            "How long the trap has to stay on ground before it can get triggered" +
+            "\n\nUnit: seconds";
+        _friendlyFire.Format("Friendly fire", _armingDuration, t => t > 0);
+        _friendlyFire.Description =
+            "Allows player traps to damage players";
+
+        CreateHeader("Trigger sizes").Description =
+            "Allows you to adjust the size of each trap's trigger";
+        using (Indent)
+        {
+            _wireTrapDepth.Format("tripwire trap");
+            _wireTrapDepth.Description =
+                "Depth (or thickness) of the tripwire trap" +
+                "\n\nUnit: in-game length units";
+            _pressureTrapRadius.Format("pressure plate");
+            _wireTrapDepth.Description =
+                "Radius of the pressure plates" +
+                "\n\nUnit: in-game length units";
+            _runicTrapRadius.Format("runic trap");
+            _wireTrapDepth.Description =
+                "Radius of the runic traps" +
+                "\n\nUnit: in-game length units";
+        }
+    }
+    #endregion
+
+    #region Utility
     private static void ResetColor(DeployableTrap __instance)
     {
         if (__instance.CurrentTrapType == DeployableTrap.TrapType.Runic)
@@ -79,11 +88,19 @@ public class Traps : AMod
         }
     }
     private static ParticleSystem.MainModule GetRunicTrapParticleSystemMainModule(DeployableTrap __instance)
-    => __instance.CurrentVisual.GetComponentInChildren<ParticleSystem>().main;
+        => __instance.CurrentVisual.GetComponentInChildren<ParticleSystem>().main;
     private static Material GetTrapMainMaterial(DeployableTrap __instance)
-    => __instance.CurrentVisual.FindChild("TrapVisual").GetComponentInChildren<MeshRenderer>().material;
+        => __instance.CurrentVisual.FindChild("TrapVisual").GetComponentInChildren<MeshRenderer>().material;
 
-    // Hooks
+    private static Color TRAP_START_COLOR = Color.white;
+    private static Color TRAP_TRANSITION_COLOR = Color.yellow;
+    private static Color TRAP_ARMED_COLOR = Color.red;
+    private static Color RUNIC_TRAP_START_COLOR = new(1f, 1f, 1f, 0f);
+    private static Color RUNIC_TRAP_TRANSITION_COLOR = new(1f, 1f, 0.05f, 0.05f);
+    private static Color RUNIC_TRAP_ARMED_COLOR = new(1f, 0.05f, 0f, 1f);
+    #endregion
+
+    #region Hooks
     [HarmonyPostfix, HarmonyPatch(typeof(DeployableTrap), nameof(DeployableTrap.StartInit))]
     private static void DeployableTrap_StartInit_Post(DeployableTrap __instance)
     {
@@ -91,7 +108,7 @@ public class Traps : AMod
         Character.Factions[] factions = __instance.TargetFactions;
         Character.Factions playerFaction = Character.Factions.Player;
         Character.Factions noneFaction = Character.Factions.NONE;
-        if (_trapsFriendlyFire && !factions.Contains(playerFaction))
+        if (_friendlyFire && !factions.Contains(playerFaction))
         {
             if (factions.Contains(noneFaction))
                 factions[factions.IndexOf(noneFaction)] = playerFaction;
@@ -101,14 +118,12 @@ public class Traps : AMod
                 factions.SetLast(playerFaction);
             }
         }
-        else if (!_trapsFriendlyFire && factions.Contains(playerFaction))
+        else if (!_friendlyFire && factions.Contains(playerFaction))
             factions[factions.IndexOf(playerFaction)] = noneFaction;
 
         // Rune trap only
-        #region quit
         if (__instance.CurrentTrapType != DeployableTrap.TrapType.Runic)
             return;
-        #endregion
 
         // Cache
         ParticleSystem.MainModule particleSystemMain = __instance.CurrentVisual.GetComponentInChildren<ParticleSystem>().main;
@@ -123,8 +138,8 @@ public class Traps : AMod
         float setupTime = Time.time;
         __instance.ExecuteUntil
         (
-            () => Time.time - setupTime >= _trapsArmDelay,
-            () => particleSystemMain.startColor = Utils.Lerp3(RUNIC_TRAP_START_COLOR, RUNIC_TRAP_TRANSITION_COLOR, RUNIC_TRAP_ARMED_COLOR, (Time.time - setupTime) / _trapsArmDelay),
+            () => Time.time - setupTime >= _armingDuration,
+            () => particleSystemMain.startColor = Utils.Lerp3(RUNIC_TRAP_START_COLOR, RUNIC_TRAP_TRANSITION_COLOR, RUNIC_TRAP_ARMED_COLOR, (Time.time - setupTime) / _armingDuration),
             () =>
             {
                 particleSystemMain.startColor = RUNIC_TRAP_ARMED_COLOR;
@@ -136,10 +151,8 @@ public class Traps : AMod
     [HarmonyPostfix, HarmonyPatch(typeof(DeployableTrap), nameof(DeployableTrap.OnReceiveArmTrap))]
     private static void DeployableTrap_OnReceiveArmTrap_Post(DeployableTrap __instance)
     {
-        #region quit
         if (__instance.CurrentTrapType == DeployableTrap.TrapType.Runic)
             return;
-        #endregion
 
         // Cache
         Collider collider = __instance.m_interactionToggle.m_interactionCollider;
@@ -158,8 +171,8 @@ public class Traps : AMod
         float setupTime = Time.time;
         __instance.ExecuteUntil
         (
-            () => Time.time - setupTime >= _trapsArmDelay,
-            () => material.color = Utils.Lerp3(TRAP_START_COLOR, TRAP_TRANSITION_COLOR, TRAP_ARMED_COLOR, (Time.time - setupTime) / _trapsArmDelay),
+            () => Time.time - setupTime >= _armingDuration,
+            () => material.color = Utils.Lerp3(TRAP_START_COLOR, TRAP_TRANSITION_COLOR, TRAP_ARMED_COLOR, (Time.time - setupTime) / _armingDuration),
             () =>
             {
                 material.color = TRAP_ARMED_COLOR;
@@ -170,10 +183,10 @@ public class Traps : AMod
 
     [HarmonyPrefix, HarmonyPatch(typeof(DeployableTrap), nameof(DeployableTrap.CleanUp))]
     private static void DeployableTrap_CleanUp_Pre(DeployableTrap __instance)
-    => ResetColor(__instance);
-
+        => ResetColor(__instance);
 
     [HarmonyPrefix, HarmonyPatch(typeof(DeployableTrap), nameof(DeployableTrap.Disassemble))]
     private static void DeployableTrap_Disassemble_Pre(DeployableTrap __instance)
-    => ResetColor(__instance);
+        => ResetColor(__instance);
+    #endregion
 }
