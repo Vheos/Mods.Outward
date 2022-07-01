7574c63332367659f6c3ed50f68c9a0ccc2b74d8
Vheos <Vheos777@gmail.com>
2022-07-01 02:59:07 +0200

    Refreshed mod `AI`

--- Code/Mods/Combat/AI.cs
+++ Code/Mods/Combat/AI.cs
@@ -3,87 +3,26 @@ using System.Collections;
 
 public class AI : AMod
 {
-    #region Constants
-    private const float HUMAN_COLLISION_RADIUS = 0.4f;
-    private static readonly Dictionary<FactionGroup, Character.Factions[]> FACTION_GROUPS_BY_ENUM = new()
-    {
-        [FactionGroup.HumansAndNonHostileMonsters] = new[]
-        {
-            Character.Factions.Bandits,
-            Character.Factions.Deer,
-        },
-        [FactionGroup.HostileMonsters] = new[]
-        {
-            Character.Factions.Mercs,
-            Character.Factions.Tuanosaurs,
-            Character.Factions.Hounds,
-            Character.Factions.Merchants,
-            Character.Factions.Golden,
-            Character.Factions.CorruptionSpirit,
-        },
-    };
-    #endregion
-    #region Enums
-    [Flags]
-    private enum FactionGroup
-    {
-        HumansAndNonHostileMonsters = 1 << 1,
-        HostileMonsters = 1 << 2,
-    }
-    #endregion
-
-    // Settings
+    #region Settings
     private static ModSetting<int> _enemyDetectionModifier;
     private static ModSetting<FactionGroup> _preventInfighting;
     private static ModSetting<int> _walkTowardsPlayerOnSpawn;
-    private static ModSetting<int> _changeTargetOnHit;
-    private static ModSetting<bool> _changeTargetWhenTooFar;
-    private static ModSetting<Vector2> _changeTargetCheckInterval;
-    private static ModSetting<float> _changeTargetCurrentToNearestRatio;
-    private static ModSetting<bool> _changeTargetDetectAllPlayers;
+    private static ModSetting<int> _retargetOnHit;
+    private static ModSetting<bool> _retargetWhenTooFar;
+    private static ModSetting<Vector2> _retargetCheckInterval;
+    private static ModSetting<float> _retargetCurrentToNearestRatio;
+    private static ModSetting<bool> _retargetDetectAllPlayers;
     protected override void Initialize()
     {
         _enemyDetectionModifier = CreateSetting(nameof(_enemyDetectionModifier), 0, IntRange(-100, +100));
         _preventInfighting = CreateSetting(nameof(_preventInfighting), (FactionGroup)0);
-        _changeTargetOnHit = CreateSetting(nameof(_changeTargetOnHit), 50, IntRange(0, 100));
+        _retargetOnHit = CreateSetting(nameof(_retargetOnHit), 50, IntRange(0, 100));
         _walkTowardsPlayerOnSpawn = CreateSetting(nameof(_walkTowardsPlayerOnSpawn), 50, IntRange(0, 100));
-        _changeTargetWhenTooFar = CreateSetting(nameof(_changeTargetWhenTooFar), false);
-        _changeTargetCheckInterval = CreateSetting(nameof(_changeTargetCheckInterval), new Vector2(0f, 2f));
-        _changeTargetCurrentToNearestRatio = CreateSetting(nameof(_changeTargetCurrentToNearestRatio), 1.5f, FloatRange(1f, 2f));
-        _changeTargetDetectAllPlayers = CreateSetting(nameof(_changeTargetDetectAllPlayers), true);
+        _retargetWhenTooFar = CreateSetting(nameof(_retargetWhenTooFar), false);
+        _retargetCheckInterval = CreateSetting(nameof(_retargetCheckInterval), new Vector2(0f, 2f));
+        _retargetCurrentToNearestRatio = CreateSetting(nameof(_retargetCurrentToNearestRatio), 1.5f, FloatRange(1f, 2f));
+        _retargetDetectAllPlayers = CreateSetting(nameof(_retargetDetectAllPlayers), true);
     }
-    protected override void SetFormatting()
-    {
-        _enemyDetectionModifier.Format("Enemy detection modifier");
-        _enemyDetectionModifier.Description = "at +100% enemies will detect you from twice the vanilla distance, and in a 90 degrees cone\n" +
-                                              "at -50% all ranges and angles will be halved\n" +
-                                              "at -100% they will be effectively blind and deaf";
-        _preventInfighting.Format("Prevent infighting between");
-        _preventInfighting.Description = "Humans and non hostile monsters:\n" +
-                                         "most human enemies (bandits) and monster enemies that aren't hostile at first sight (pearlbirds, deers) will ignore each other\n" +
-                                         "\n" +
-                                         "Hostile monsters\n" +
-                                         "most hostile monsters (mantises, hive lords, assassin bugs) will ignore each other";
-        _walkTowardsPlayerOnSpawn.Format("Walk towards player on spawn");
-        _walkTowardsPlayerOnSpawn.Description = "Chance that enemies will walk in the nearest player's direction when spawned";
-        _changeTargetOnHit.Format("Change target on hit");
-        _changeTargetOnHit.Description = "Chance that enemies will target the most recent attacker\n" +
-                                         "at 0%, enemies will never change their first targets";
-        _changeTargetWhenTooFar.Format("Change target when too far");
-        _changeTargetWhenTooFar.Description = "When enemy's current target retreats too far, the enemy will change to a closer one\n" +
-                                              "Only detected characters and actual attackers are taken into account";
-        using (Indent)
-        {
-            _changeTargetCheckInterval.Format("Check interval", _changeTargetWhenTooFar);
-            _changeTargetCheckInterval.Description = "After a distance check, enemies will wait for a random duration between x and y before performing another check";
-            _changeTargetCurrentToNearestRatio.Format("Minimum target-nearest ratio", _changeTargetWhenTooFar);
-            _changeTargetCurrentToNearestRatio.Description = "Enemies will change target only if their current target is this many times further away then their nearest attacker";
-            _changeTargetDetectAllPlayers.Format("Detect all players", _changeTargetWhenTooFar);
-            _changeTargetDetectAllPlayers.Description = "When enemies detect any player, they will become aware of other players as well";
-        }
-    }
-    protected override string SectionOverride
-    => ModSections.Combat;
     protected override void LoadPreset(string presetName)
     {
         switch (presetName)
@@ -92,17 +31,71 @@ public class AI : AMod
                 ForceApply();
                 _enemyDetectionModifier.Value = +33;
                 _preventInfighting.Value = (FactionGroup)~0;
-                _changeTargetOnHit.Value = 0;
+                _retargetOnHit.Value = 0;
                 _walkTowardsPlayerOnSpawn.Value = 0;
-                _changeTargetWhenTooFar.Value = true;
-                _changeTargetCheckInterval.Value = new(0f, 2f);
-                _changeTargetCurrentToNearestRatio.Value = 1.2f;
-                _changeTargetDetectAllPlayers.Value = true;
+                _retargetWhenTooFar.Value = true;
+                _retargetCheckInterval.Value = new(0f, 2f);
+                _retargetCurrentToNearestRatio.Value = 1.2f;
+                _retargetDetectAllPlayers.Value = true;
                 break;
         }
     }
+    #endregion
 
-    // Utility
+    #region Formatting
+    protected override string SectionOverride
+        => ModSections.Combat;
+    protected override string Description
+        => "• Adjust enemy detection" +
+        "\n• Stop infighting between enemies" +
+        "\n• Adjust enemies' retargeting behaviour";
+    protected override void SetFormatting()
+    {
+        _enemyDetectionModifier.Format("Enemy detection modifier");
+        _enemyDetectionModifier.Description =
+            "How good the enemies are at detecting you" +
+            "\nat +100% enemies will detect you from twice the original distance, and in a 90 degrees cone" +
+            "\nat -50% all enemies' detectionranges and angles will be halved" +
+            "\nat -100% enemies will be effectively blind and deaf" +
+            "\n\nUnit: arbitrary linear scale";
+        _preventInfighting.Format("Prevent infighting between...");
+        _preventInfighting.Description =
+            $"{FactionGroup.HumansAndNonHostileMonsters}:" +
+            $"\nmost humans (eg. bandits) and non-hostile monsters (eg. pearlbirds, deers) will ignore each other" +
+            $"\n\n{FactionGroup.HostileMonsters}:" +
+            $"\nmost hostile monsters (eg. mantises, hive lords, assassin bugs) will ignore each other";
+        _walkTowardsPlayerOnSpawn.Format("Walk towards player on spawn");
+        _walkTowardsPlayerOnSpawn.Description =
+            "How likely enemies are to walk towards the nearest player when they spawn" +
+            "\n\nUnit: percent chance";
+        _retargetOnHit.Format("Chance to retarget on hit");
+        _retargetOnHit.Description =
+            "How likely enemies are to start targeting the attacker when attacked" +
+            "\n\nUnit: percent chance";
+        _retargetWhenTooFar.Format("Retarget when too far");
+        _retargetWhenTooFar.Description =
+            "Allows enemies to change their current target when it's too far away" +
+            "\nOnly detected characters and actual attackers are taken into account";
+        using (Indent)
+        {
+            _retargetCurrentToNearestRatio.Format("Current-to-nearest ratio", _retargetWhenTooFar);
+            _retargetCurrentToNearestRatio.Description =
+                "How far away enemies have to be from their current target to switch to a closer one" +
+                "\nEnemies will switch only if their current target is this many times further away then the closest one" +
+                "\n\nUnit: ratio between distance to the current target and to the closest valid target";
+            _retargetCheckInterval.Format("Check interval", _retargetWhenTooFar);
+            _retargetCheckInterval.Description =
+                "How often enemies will attempt to switch to a closer target" +
+                "\nEach interval is a random value between x and y" +
+                "\n\nUnit: seconds";
+            _retargetDetectAllPlayers.Format("Detect all players", _retargetWhenTooFar);
+            _retargetDetectAllPlayers.Description =
+                "Allows enemies to detect all players when they detect any";
+        }
+    }
+    #endregion
+
+    #region Utility
     public static void TryRetarget(CharacterAI ai)
     {
         Character enemy = ai.Character;
@@ -127,7 +120,7 @@ public class AI : AMod
             }
 
         //Log.Debug($"{enemy.Name} - distance: {currentDistance:F2} / {nearestDistance:F2} = {currentDistance / nearestDistance:F2}");
-        if (currentDistance / nearestDistance >= _changeTargetCurrentToNearestRatio)
+        if (currentDistance / nearestDistance >= _retargetCurrentToNearestRatio)
         {
             //Log.Debug($"\tswitching to {nearestAttacker.Name}");
             enemy.TargetingSystem.SwitchTarget(nearestAttacker.LockingPoint);
@@ -146,18 +139,42 @@ public class AI : AMod
             }
 
             TryRetarget(ai);
-            yield return new WaitForSeconds(_changeTargetCheckInterval.Value.RandomRange());
+            yield return new WaitForSeconds(_retargetCheckInterval.Value.RandomRange());
         }
     }
 
+    private static readonly Dictionary<FactionGroup, Character.Factions[]> FACTION_GROUPS_BY_ENUM = new()
+    {
+        [FactionGroup.HumansAndNonHostileMonsters] = new[]
+        {
+            Character.Factions.Bandits,
+            Character.Factions.Deer,
+        },
+        [FactionGroup.HostileMonsters] = new[]
+        {
+            Character.Factions.Mercs,
+            Character.Factions.Tuanosaurs,
+            Character.Factions.Hounds,
+            Character.Factions.Merchants,
+            Character.Factions.Golden,
+            Character.Factions.CorruptionSpirit,
+        },
+    };
+    [Flags]
+    private enum FactionGroup
+    {
+        HumansAndNonHostileMonsters = 1 << 1,
+        HostileMonsters = 1 << 2,
+    }
+    #endregion
+
+    #region Hooks
     // Prevent infighting
     [HarmonyPrefix, HarmonyPatch(typeof(TargetingSystem), nameof(TargetingSystem.InitTargetableFaction))]
     private static bool TargetingSystem_InitTargetableFaction_Pre(TargetingSystem __instance)
     {
-        #region quit
         if (_preventInfighting.Value == 0)
             return true;
-        #endregion
 
         // Cache
         Character.Factions characterFaction = __instance.m_character.Faction;
@@ -190,10 +207,8 @@ public class AI : AMod
     [HarmonyPostfix, HarmonyPatch(typeof(AIPreset), nameof(AIPreset.ApplyToCharAI))]
     private static void AIPreset_ApplyToCharAI_Post(AIPreset __instance, CharacterAI _charAI)
     {
-        #region quit
         if (_enemyDetectionModifier == 0)
             return;
-        #endregion
 
         float multiplier = 1 + _enemyDetectionModifier / 100f;
         foreach (var detection in _charAI.GetComponentsInChildren<AICEnemyDetection>(true))
@@ -217,20 +232,18 @@ public class AI : AMod
 
     [HarmonyPrefix, HarmonyPatch(typeof(AISquadSpawnPoint), nameof(AISquadSpawnPoint.SpawnSquad))]
     private static void AISquadSpawnPoint_SpawnSquad_Pre(AISquadSpawnPoint __instance)
-    => __instance.ChanceToWanderTowardsPlayers = _walkTowardsPlayerOnSpawn;
+        => __instance.ChanceToWanderTowardsPlayers = _walkTowardsPlayerOnSpawn;
 
     [HarmonyPrefix, HarmonyPatch(typeof(AICEnemyDetection), nameof(AICEnemyDetection.Init))]
     private static void AICEnemyDetection_Init_Pre(AICEnemyDetection __instance)
-    => __instance.ChanceToSwitchTargetOnHurt = _changeTargetOnHit;
+        => __instance.ChanceToSwitchTargetOnHurt = _retargetOnHit;
 
     // Change target when far
     [HarmonyPostfix, HarmonyPatch(typeof(CharacterAI), nameof(CharacterAI.SwitchAiState))]
     private static void CharacterAI_SwitchAiState_Post(CharacterAI __instance)
     {
-        #region quit
-        if (!_changeTargetWhenTooFar)
+        if (!_retargetWhenTooFar)
             return;
-        #endregion
 
         AIState previousState = __instance.AiStates[__instance.m_previousStateID];
         AIState currentState = __instance.AiStates[__instance.m_currentStateID];
@@ -249,21 +262,15 @@ public class AI : AMod
     [HarmonyPostfix, HarmonyPatch(typeof(AICEnemyDetection), nameof(AICEnemyDetection.Detected))]
     private static void AICEnemyDetection_Detected_Post(AICEnemyDetection __instance, LockingPoint _point)
     {
-        #region quit
-        if (!_changeTargetWhenTooFar || !_point.OwnerChar.TryNonNull(out var target))
+        if (!_retargetWhenTooFar
+        || !_point.OwnerChar.TryNonNull(out var target))
             return;
-        #endregion
 
         Character enemy = __instance.m_characterAI.Character;
         enemy.AddLastDealer(target.UID);
-        if (_changeTargetDetectAllPlayers && target.IsPlayer())
+        if (_retargetDetectAllPlayers && target.IsPlayer())
             foreach (var player in Players.Local)
                 enemy.AddLastDealer(player.Character.UID);
     }
+    #endregion
 }
-
-/*
-*         static private ModSetting<float> _changeMinimumDistance;
-    _changeMinimumDistance = CreateSetting(nameof(_changeMinimumDistance), 1f, FloatRange(HUMAN_COLLISION_RADIUS* 2, 5f));
-        _changeMinimumDistance.Format("Minimum distance", _changeTargetWhenTooFar);
-*/
